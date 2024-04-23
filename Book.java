9f2e3e9678376d56cb19501e18851c70aa81da04
M.S. Jang <ms3814@naver.com>
2024-04-23 16:58:20 +0900

    테스트

---
+++ Book.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Book {
+	private String title;
+	private String author;
+	private boolean isAvailable;
+	public Book(String title, String author, boolean isAvailable) {
+		 this.title = title.isEmpty() ? "Unknown" : title;
+		 this.author = author.isEmpty() ? "Unknown" : author;
+		 this.isAvailable = isAvailable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	@Override
+	public String toString() {
+	    return String.format("%s | %s | %s",
+	                         title != null ? title : "unknown",
+	                         author != null ? author : "unknown",
+	                         isAvailable ? "대출 가능" : "대출 불가");
+	}
+	
+	
+}
